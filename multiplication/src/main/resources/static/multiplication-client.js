15ef792cde56b3986b4174821e4d5bf866ad37c6
Javad Alimohammadi <bs.alimohammadi@gmail.com>
2019-09-08 23:21:58 +0430

    change js

--- multiplication/src/main/resources/static/multiplication-client.js
+++ multiplication/src/main/resources/static/multiplication-client.js
@@ -30,7 +30,7 @@ $(document).ready(function () {
 
     updateMultiplication();
 
-    $("#attempt-form").submit(function (event) {
+    $("#attempt-form").submit(function( event ) {
 
         // Don't submit the form normally
         event.preventDefault();
@@ -38,12 +38,12 @@ $(document).ready(function () {
         // Get some values from elements on the page
         var a = $('.multiplication-a').text();
         var b = $('.multiplication-b').text();
-        var $form = $(this),
-            attempt = $form.find("input[name='result-attempt']").val(),
-            userAlias = $form.find("input[name='user-alias']").val();
+        var $form = $( this ),
+          attempt = $form.find( "input[name='result-attempt']" ).val(),
+          userAlias = $form.find( "input[name='user-alias']" ).val();
 
         // Compose the data in the format that the API is expecting
-        var data = {user: {alias: userAlias}, multiplication: {factorA: a, factorB: b}, resultAttempt: attempt};
+        var data = { user: { alias: userAlias}, multiplication: {factorA: a, factorB: b}, resultAttempt: attempt};
 
         // Send the data using post
         $.ajax({
@@ -53,17 +53,17 @@ $(document).ready(function () {
             contentType: "application/json; charset=utf-8",
             dataType: "json",
             async: false,
-            success: function (result) {
-                if (result.correct) {
+            success: function(result){
+                if(result.correct) {
                     $('.result-message').empty().append("The result is correct! Congratulations!");
                 } else {
-                    $('.result-message').empty().append("Oops that's not correct! But keep trying!");
+                    $('.result-message').empty().append("Ooops that's not correct! But keep trying!");
                 }
             }
         });
 
         updateMultiplication();
-    });
 
-    updateStats(userAlias);
+        updateStats(userAlias);
+    });
 });
