3acbfa443d9bed7a33cd30aa5d0641ef6351990e
Javad Alimohammadi <bs.alimohammadi@gmail.com>
2019-09-13 23:09:07 +0430

    add api gateway

---
+++ multiplication/src/main/java/microservices/book/multiplication/config/WebConfiguration.java
@@ -0,0 +1,30 @@
+package microservices.book.multiplication.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author Javad Alimohammadi<bs.alimohammadi@gmail.com>
+ */
+
+@Configuration
+@EnableWebMvc
+public class WebConfiguration implements WebMvcConfigurer {
+    /**
+     * Enables Cross-Origin Resource Sharing (CORS)
+     * More info: http://docs.spring.io/
+     * spring/docs/current/spring-framework-reference/html
+     * /cors.html
+     *
+     * @param registry
+     */
+
+    @Override
+    public void addCorsMappings(final CorsRegistry registry) {
+        registry.addMapping("/**");
+    }
+
+
+}
