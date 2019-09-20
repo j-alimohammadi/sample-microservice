76f569c4f14e3ef10c793ea1ecd2b80df4490fab
Javad Alimohammadi <bs.alimohammadi@gmail.com>
2019-09-20 22:43:04 +0430

    add service registry

--- multiplication/src/main/java/microservices/book/multiplication/controller/MultiplicationController.java
+++ multiplication/src/main/java/microservices/book/multiplication/controller/MultiplicationController.java
@@ -1,9 +1,10 @@
 package microservices.book.multiplication.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import microservices.book.multiplication.domain.Multiplication;
 import microservices.book.multiplication.service.MultiplicationService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,20 +14,24 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/multiplications")
+@Slf4j
 public class MultiplicationController {
 
     private final MultiplicationService multiplicationService;
+    private final int serverPort;
 
-    public MultiplicationController(MultiplicationService multiplicationService) {
+    public MultiplicationController(MultiplicationService multiplicationService,
+                                    @Value("${server.port}") int serverPort) {
         this.multiplicationService = multiplicationService;
+        this.serverPort = serverPort;
     }
 
 
     @GetMapping("/random")
     public Multiplication getRandomMultiplication() {
+        log.info("Generating a random multiplication from server @ {}", serverPort);
         return multiplicationService.createRandomMultiplication();
     }
-
 
 
 }
